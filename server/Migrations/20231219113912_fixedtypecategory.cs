9fd936b066d251fec5a506eff2b94d9596caf3a7
BiffJonas <johannesjoh00@gmail.com>
2023-12-19 12:45:59 +0100

    changed how you add alternatives

---
+++ server/Migrations/20231219113912_fixedtypecategory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedtypecategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Catagory",
+                table: "AltCards",
+                newName: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "AltCards",
+                newName: "Catagory");
+        }
+    }
+}
